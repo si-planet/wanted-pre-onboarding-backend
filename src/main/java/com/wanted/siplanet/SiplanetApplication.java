46f6b654189f149cbe16e62bd7cdada9a720cd79
Sooin Ju <sooin.ju@gmail.com>
2024-08-06 18:02:20 +0900

    0. DB 및 환경설정 세팅

---
+++ src/main/java/com/wanted/siplanet/SiplanetApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.siplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiplanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/siplanet/SiplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.siplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
